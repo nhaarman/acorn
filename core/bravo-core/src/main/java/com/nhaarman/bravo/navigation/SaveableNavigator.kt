4e0056c5f321db4b506358613ab0c332527df11c
Niek Haarman <haarman.niek@gmail.com>
2018-09-04 16:11:17 +0200

    Remove unused RestoreableNavigator interface
    This interface isn't used anyway to favor restoring directly
    in the constructor.

--- core/bravo-core/src/main/java/com/nhaarman/bravo/navigation/SaveableNavigator.kt
+++ core/bravo-core/src/main/java/com/nhaarman/bravo/navigation/SaveableNavigator.kt
@@ -10,12 +10,3 @@ interface SaveableNavigator {
     /** Save instance state. */
     fun saveInstanceState(): NavigatorState
 }
-
-/**
- * Indicates that implementers can have their instance state saved and restored.
- */
-interface RestorableNavigator : SaveableNavigator {
-
-    /** Restore given instance state. */
-    fun restoreInstanceState(bundle: NavigatorState)
-}
