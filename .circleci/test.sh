9e3b0bae27787ccb3333dce1d7e9dddba9e4ddf0
Niek Haarman <haarman.niek@gmail.com>
2018-09-29 19:35:21 +0200

    Introduce ext-bravo-android-lifecycle
    This includes a LifecycleScene that implements
    `android.arch.lifecycle.LifecycleOwner`, and can
    be used as such when using LiveData.

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -5,12 +5,21 @@
     ktlint \
     --rerun-tasks
 
-./gradlew :samples:hello-world:lintRelease \
+./gradlew  \
+    :ext-bravo-android:lint \
+    :ext-bravo-android-testing:lint \
+    :ext-bravo-android-timber:lint \
+    :ext-bravo-android-lifecycle:lint
+
+./gradlew  \
+    :samples:hello-world:lintRelease \
     :samples:hello-navigation:lintRelease \
     :samples:hello-staterestoration:lintRelease \
     :samples:hello-transitionanimation:lintRelease \
     :samples:hello-startactivity:lintRelease \
-    :samples:hello-sharedata:lintRelease \
+    :samples:hello-sharedata:lintRelease
+
+./gradlew  \
     :samples:notes-app:aac-navigation:lintRelease \
     :samples:notes-app:cicerone:lintRelease \
     :samples:notes-app:conductor:lintRelease \
