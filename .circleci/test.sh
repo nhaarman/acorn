6bef9bfae4d44e242aa032b291d99d8b9472ee1d
Niek Haarman <haarman.niek@gmail.com>
2018-10-09 11:53:37 +0200

    Use a single worker on CI
    In the hope this solves some memory issues

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -3,28 +3,24 @@
 ./gradlew clean \
     test \
     ktlint \
-    --rerun-tasks
-
-./gradlew  \
+    \
     :ext-bravo-android:lint \
     :ext-bravo-android-testing:lint \
     :ext-bravo-android-timber:lint \
-    :ext-bravo-android-lifecycle:lint
-
-./gradlew  \
+    :ext-bravo-android-lifecycle:lint \
+    \
     :samples:hello-world:lintRelease \
     :samples:hello-navigation:lintRelease \
     :samples:hello-staterestoration:lintRelease \
     :samples:hello-transitionanimation:lintRelease \
     :samples:hello-startactivity:lintRelease \
-    :samples:hello-sharedata:lintRelease
-
-./gradlew  \
+    :samples:hello-sharedata:lintRelease \
+    \
     :samples:notes-app:aac-navigation:lintRelease \
     :samples:notes-app:cicerone:lintRelease \
     :samples:notes-app:conductor:lintRelease \
     :samples:notes-app:mosby:lintRelease \
     :notes-app-bravo-android:lintRelease \
-    --rerun-tasks
-
-./gradlew publishToMavenLocal --rerun-tasks
+    \
+    publishToMavenLocal \
+    --max-workers=1 --rerun-tasks
